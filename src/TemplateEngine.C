c87c0db3f41b564f0814689293776298a4bbd941
adam <adam.lange@langeturbo.com>
2016-04-02 23:56:33 -0500

    implementation of operations in TemplateEngine

--- src/TemplateEngine.C
+++ src/TemplateEngine.C
@@ -12,6 +12,8 @@
 #include <list>
 #include <iostream>
 #include "TopoDS_Edge.hxx"
+#include "PostProcessor.h"
+#define _USE_MATH_DEFINES
 
 TemplateEngine::TemplateEngine(const std::string& input)
 {
@@ -79,22 +81,77 @@ TemplateEngine::run()
     else if (opType == "NormalCrossingHorizontalMarker")
     {
       std::cout<<"nchm"<<std::endl;
+      NormalCrossingHorizontalMarker nchm;
+      nchm.setMaxDz((*it)["maxDz"].asFloat());
+      for (std::list<Slice>::iterator slice_it = slices.begin();
+           slice_it != slices.end();
+           ++slice_it)
+      {
+        slice_it->refine(nchm);
+      }
     }
     else if (opType == "SurfaceNormalSplitter")
     {
       std::cout<<"sns"<<std::endl;
+      SurfaceNormalSplitter sns;
+      std::list<Slice> newSlices;
+      double x0 = (*it)["xMin"].asFloat();
+      double y0 = (*it)["yMin"].asFloat();
+      double z0 = (*it)["zMin"].asFloat();
+      double x1 = (*it)["xMax"].asFloat();
+      double y1 = (*it)["yMax"].asFloat();
+      double z1 = (*it)["zMax"].asFloat();
+      sns.setXLimits(x0,x1);
+      sns.setYLimits(y0,y1);
+      sns.setZLimits(z0,z1);
+      for (std::list<Slice>::iterator slice_it = slices.begin();
+           slice_it != slices.end();
+           ++slice_it)
+      {
+        newSlices.splice(newSlices.end(),slice_it->split(sns));
+      }
+      slices = newSlices;
     }
     else if (opType == "TraverseAngleSplitter")
     {
-        std::cout<<"tas"<<std::endl;
+      std::cout<<"tas"<<std::endl;
+      std::list<Slice> newSlices;
+      TraverseAngleSplitter tas;
+      double alpha = (*it)["maxTraverseAngle"].asFloat()*M_PI/180.0;
+      tas.setMaxTraverseAngle(alpha);
+      for (std::list<Slice>::iterator slice_it = slices.begin();
+           slice_it != slices.end();
+           ++slice_it)
+      {
+        newSlices.splice(newSlices.end(),slice_it->split(tas));
+      }
+      slices = newSlices;
     }
     else if (opType == "AlphaJumpMarker")
     {
-        std::cout<<"ajm"<<std::endl;
+      std::cout<<"ajm"<<std::endl;
+      AlphaJumpMarker ajm;
+      double j = (*it)["maxAlphaJump"].asFloat()*M_PI/180.0;
+      ajm.setMaxAlphaJump(j);
+      for (std::list<Slice>::iterator slice_it = slices.begin();
+           slice_it != slices.end();
+           ++slice_it)
+      {
+        slice_it->refine(ajm);
+      }
     }
     else if (opType == "PointToPointDistanceMarker")
     {
-        std::cout<<"p2pdm"<<std::endl;
+      std::cout<<"p2pdm"<<std::endl;
+      PointToPointDistanceMarker p2pdm;
+      double d = (*it)["maxDistance"].asFloat();
+      p2pdm.setMaxDistance(d);
+      for (std::list<Slice>::iterator slice_it = slices.begin();
+           slice_it != slices.end();
+           ++slice_it)
+      {
+        slice_it->refine(p2pdm);
+      }
     }
     else
     {
@@ -104,7 +161,8 @@ TemplateEngine::run()
     
   }
 
-  std::string str;
+  PostProcessor pp(10,1/25.4,180/M_PI);
+  std::string str = pp.postProcess(slices);
   str += "poof\n";
   return str;
 }
